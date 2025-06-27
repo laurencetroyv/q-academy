82ae5685b149cf510f092779ca11b3be79b7126a
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2025-06-27 22:00:41 +0800

    ✨ feat: Add vercel analytics and speed insight

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,11 @@
 <template>
+  <Analytics/>
+  <SpeedInsights/>
   <NuxtLayout>
     <NuxtPage/>
   </NuxtLayout>
 </template>
+<script setup lang="ts">
+import {Analytics} from '@vercel/analytics/nuxt'
+import {SpeedInsights} from "@vercel/speed-insights/nuxt"
+</script>
